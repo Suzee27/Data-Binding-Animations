6e12df16ebec0f8788d8323a109eda5167857fb1
Suzee27 <suzieoborokumo@gmail.com>
2022-11-28 12:30:58 +0100

    Add project files.

---
+++ DependencyProperties.WPF/Dp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DependencyProperties.WPF
+{
+    internal class Dp : Window
+    {
+
+
+        public int Prop
+        {
+            get { return (int)GetValue(PropProperty); }
+            set { SetValue(PropProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Prop.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PropProperty =
+            DependencyProperty.Register("Prop", typeof(int), typeof(MainWindow), new PropertyMetadata(0, new PropertyChangedCallback(OnPropChanged)));
+
+
+        private static void OnPropChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        private static void OnPropChanged(DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        // attached property
+        public static int GetMyProperty(DependencyObject obj)
+        {
+            return (int)obj.GetValue(MyPropertyProperty);
+        }
+
+        public static void SetMyProperty(DependencyObject obj, int value)
+        {
+            obj.SetValue(MyPropertyProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.RegisterAttached("MyProperty", typeof(int), typeof(Window), new PropertyMetadata(0));
+    }
+}
---
+++ DependencyProperties.WPF/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DependencyProperties.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = _rectangle;
+        }
+        AnimationRectangle _rectangle = new(60, 110);
+       
+    }
+
+
+    public class AnimationRectangle
+    {
+        public AnimationRectangle(int height, int width)
+        {
+            Width = width;
+            Height = height;
+        }
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        private int width;
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+    }
+}
